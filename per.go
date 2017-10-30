f972bba15fafd9b536c1b23a20f6131fcee9b7b2
ThomasHuke <thomashuke@icloud.com>
2017-10-31 00:46:50 +0800

    change

--- per.go
+++ per.go
@@ -6,11 +6,11 @@ import (
 	"net/http"
 )
 
-func Run(url []string, t typ, d chan string) {
-	go tt(url, d)
-	for i := range d {
+func Run(url []string, t typ) {
+	ace := tt(url)
+	for _, value := range ace {
 		// dealWith处理接收到的信息。
-		message := dealWith(i)
+		message := dealWith(value)
 		//然后将信息传递给put方法，这样put就可以生成一个拥有信息的一个文件，并且这个文件默认是放置到桌面的
 		//后者是说目前我强制它放置到桌面了。
 		t.put(message)
@@ -19,7 +19,8 @@ func Run(url []string, t typ, d chan string) {
 
 //Run 作为对外暴露的主函数。这里t是一个interface的实例当然也就是一个struct，所以我们只要在输入数据的时候
 //输入Type Plain Json Md 类型的 struct即可。
-func tt(url []string, d chan string) {
+func tt(url []string) []string {
+	var a []string
 	for _, value := range url {
 		res, err := http.Get(value)
 		if err != nil {
@@ -30,7 +31,7 @@ func tt(url []string, d chan string) {
 		if err != nil {
 			fmt.Println("错误出现在资源转换的时候")
 		}
-		d <- string(body)
+		a = append(a, string(body))
 	}
-	close(d)
+	return a
 }
