2aea3c546fc586533104d7c6899800fea2e33d65
ThomasHuke <thomashuke@icloud.com>
2017-10-30 18:38:35 +0800

    成功发布，0.0.1

--- deal.go
+++ deal.go
@@ -2,5 +2,5 @@ package per
 
 //这个文件的目的就是为了处理get获得的各种资源，使用正则即可。
 func dealWith(body string) string {
-
+	return body
 }
--- per.go
+++ per.go
@@ -2,14 +2,14 @@ package per
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func Run(url []string, ty string, t typ, d chan *http.Response) {
-	c := make(chan string)
+func Run(url []string, ty string, t typ, d chan string) {
 	go tt(url, ty, t, d)
-	for i := range c {
+	for i := range d {
 		// dealWith处理接收到的信息。
 		message := dealWith(i)
 		//然后将信息传递给put方法，这样put就可以生成一个拥有信息的一个文件，并且这个文件默认是放置到桌面的
@@ -20,16 +20,21 @@ func Run(url []string, ty string, t typ, d chan *http.Response) {
 
 //Run 作为对外暴露的主函数。这里t是一个interface的实例当然也就是一个struct，所以我们只要在输入数据的时候
 //输入Type Plain Json Md 类型的 struct即可。
-func tt(url []string, ty string, t typ, d chan *http.Response) {
+func tt(url []string, ty string, t typ, d chan string) {
 	for _, value := range url {
 		res, err := http.Get(value)
+		if err != nil {
+			fmt.Println("Error! Here are some messages: ")
+			log.Fatalln(err)
+		}
 		defer res.Body.Close()
+		body, err := ioutil.ReadAll(res.Body)
 		if err != nil {
 			fmt.Println("Error! Here are some messages: ")
 			log.Fatalln(err)
 		}
 		fmt.Print("The output is :", *res)
-		d <- res
+		d <- string(body)
 	}
 	close(d)
 }
