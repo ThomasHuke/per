01f355a1e8f053cfe35d4d188706c2e3d5642a7c
ThomasHuke <thomashuke@icloud.com>
2017-10-31 01:25:03 +0800

    add

--- deal.go
+++ deal.go
@@ -1,6 +1,43 @@
 package per
 
+import (
+	"regexp"
+	"strings"
+)
+
 //这个文件的目的就是为了处理get获得的各种资源，使用正则即可。
 func dealWith(body string) string {
+	body = simpleDealwith(body)
+	body = plusDealwith(body)
 	return body
+}
+
+// 简单的处理
+func simpleDealwith(src sting) string {
+	//将HTML标签全转换成小写
+	re, _ := regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllStringFunc(src, strings.ToLower)
+
+	//去除STYLE
+	re, _ = regexp.Compile("\\<style[\\S\\s]+?\\</style\\>")
+	src = re.ReplaceAllString(src, "")
+
+	//去除SCRIPT
+	re, _ = regexp.Compile("\\<script[\\S\\s]+?\\</script\\>")
+	src = re.ReplaceAllString(src, "")
+
+	//去除所有尖括号内的HTML代码，并换成换行符
+	re, _ = regexp.Compile("\\<[\\S\\s]+?\\>")
+	src = re.ReplaceAllString(src, "\n")
+
+	//去除连续的换行符
+	re, _ = regexp.Compile("\\s{2,}")
+	src = re.ReplaceAllString(src, "\n")
+
+	return strings.TrimSpace(src)
+}
+
+// 更加复杂的处理
+func plusDealwith(s string) string {
+
 }
